0aa94326800b53d520cb3e8bdc8c7a0824ff9274
Blackwidow-sudo <bob_merlin92@web.de>
2023-12-11 21:11:32 +0100

    feat: add types directory & routes

---
+++ src/lib/routes.ts
@@ -0,0 +1,14 @@
+import type { Route } from '@types'
+
+const routes: Route[] = [
+	{
+		path: '/',
+		name: 'Home'
+	},
+	{
+		path: '/showcase',
+		name: 'Showcase'
+	}
+]
+
+export default routes
