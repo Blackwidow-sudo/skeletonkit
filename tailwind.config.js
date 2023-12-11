4e1aa40bf6fab33e8ca7fff447aaf991389df400
Blackwidow-sudo <bob_merlin92@web.de>
2023-12-11 21:09:20 +0100

    feat: install skeleton-ui & configure it

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
-import forms from '@tailwindcss/forms';
+import forms from '@tailwindcss/forms'
+import { skeleton } from '@skeletonlabs/tw-plugin'
+import { join } from 'path'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+	],
 	theme: {
 		extend: {}
 	},
-	plugins: [forms]
-};
+	plugins: [forms, skeleton({ themes: { preset: ['skeleton'] } })]
+}
